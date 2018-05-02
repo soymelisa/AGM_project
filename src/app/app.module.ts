0a06b106641048cb126e50f5b0e806ed35430531
Melisa Perez <soymelisa@yahoo.com>
2018-05-02 03:10:24 -0500

    configurando bootstrap con la librería md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AgmCoreModule } from '@agm/core';
 import { ResaltarDirective } from "./directives/resaltar.directive";
 import { ContarClicksDirective } from "./directives/contar-clicks.directive";
 
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 
 @NgModule({
   declarations: [
@@ -21,9 +24,11 @@ import { ContarClicksDirective } from "./directives/contar-clicks.directive";
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBBKujuwqVevE9DUO6OhofIzrWInJq6NxY'
 //      apiKey: 'AIzaSyBAMtnq2WPv7PYc00YZHHiruBs7l-Xd0xM'
-    })
+    }),
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
+  ,schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
