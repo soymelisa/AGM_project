53b33087af8dfcb21c8527f1e90a2da87619b3d4
Melisa Perez <soymelisa@yahoo.com>
2018-05-02 00:14:34 -0500

    La directiva contar-clicks probando Host Listeners vienco que con los tres parámetros, el evento, el objetivo del evento y el código a ejecutar, funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,12 @@ export class AppComponent {
   nombre:string = '';
   apellido:string = '';
   lugares:any = [
-    {nombre:'Florería la Gardenia'},
-    {nombre:'Donas la pasadita'},
-    {nombre:'Veterinaria Huellitas Felices'},
-    {nombre:'Florería la Gardenia'},
-    {nombre:'Donas la pasadita'},
-    {nombre:'Veterinaria Huellitas Felices'}
+    {plan: 'pagado', cercania: 1, distancia: 1, active: true, nombre: 'El Pendúlo'},
+    {plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas en Álvaro Obregón'},
+    {plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre: 'Metrobus Álvaro Obregón'},
+    {plan: 'gratuito', cercania: 3, distancia: 10, active: false, nombre: 'Florería sobre Insurgentes'},
+    {plan: 'pagado', cercania: 3, distancia: 35, active: true, nombre: 'Metro Chilpancingo'},
+    {plan: 'gratuito', cercania: 3, distancia: 120, active: false, nombre: 'Casa'}
   ];
   lat: number = 19.4181457;
   lng: number = -99.1615951;
